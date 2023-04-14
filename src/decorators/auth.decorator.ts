5ef35af5beefa57a9235646b71effccdf99c90f6
Maximiliano Guaymas <maxikasta480@gmail.com>
2023-04-14 02:55:26 -0300

    changesss

--- src/decorators/auth.decorator.ts
+++ src/decorators/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { UseGuards, applyDecorators } from "@nestjs/common";
 import { ApiBearerAuth } from "@nestjs/swagger";
 import { ACGuard, Role, UseRoles } from "nest-access-control";
-import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 
 export function Auth(...roles: Role[]) {
